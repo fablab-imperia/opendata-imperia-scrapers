254ea9df40f50b1413ecc933b897200571d57482
Stefano Semeria <stefano.semeria@gmail.com>
2021-05-22 12:33:27 +0200

    some generic method

---
+++ scrapers/scraper_base.py
@@ -0,0 +1,13 @@
+import abc
+
+class Dataset:
+    parameters: dict[str,str]
+    fields: dict
+    primary_key: list[str]
+    records: list[dict]
+
+    
+class ScraperBase(abc.ABC):
+    @abc.abstractmethod
+    def scrape(url: str) -> Dataset:
+        pass
---
+++ scrapers/synchronizer.py
@@ -0,0 +1,5 @@
+from scraper_base import ScraperBase
+
+class DatasetSynchronizer:
+    def __init__(self, scraper: ScraperBase):
+        pass
